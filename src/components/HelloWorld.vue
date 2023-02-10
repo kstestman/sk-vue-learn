b1da021b888f1d7a6869a52e52ef73ee0b5a6590
kstestman <k-seiji@daiichi-kogyo.co.jp>
2023-02-10 15:24:52 +0900

    test3

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Vuetify1231
+          Vuetify12312
         </h1>
 
         <p class="subheading font-weight-regular">
